185bc4942a60d47e2840f07993eac40a6ecfc18b
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-02-12 12:20:15 +0900

    update cli.py

---
+++ brain_games/cli.py
@@ -0,0 +1,14 @@
+import prompt
+  
+
+def welcome_user():
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}') 
+    
+
+def main():
+    welcome_user()
+
+
+if __name__ == "__main__":
+    main()
