40ddb74b41b036bb8ec848eaa2989b6f2f909955
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-02-15 07:40:27 +0900

    add file brain_even.py

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+from brain_games.cli import welcome_user
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    welcome_user()
+    print("This is even")
+
+
+if __name__ == '__main__':
+    main()
