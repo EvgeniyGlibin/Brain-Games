0584539ec9dcf2f85c95222b64984877eae3c435
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-02-16 07:56:40 +0900

    change the greeting

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,8 +2,9 @@ import prompt
 
 
 def welcome_user():
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}')
+    print("Welcome to the Brain Games!")
+    #name = prompt.string('May I have your name? ')
+    #print(f'Hello, {name}')
 
 
 def main():
