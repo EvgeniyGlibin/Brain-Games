7ffb05b61c03ede153b4050e527384d5bb25492a
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-02-18 12:47:12 +0900

    change logic games calc

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 from brain_games.cli import welcome_user
-
+from brain_games.cli import congratulate_user
 
 def main():
     welcome_user()
     print("This is brain-calc")
+    congratulate_user()
 
 
 if __name__ == '__main__':
