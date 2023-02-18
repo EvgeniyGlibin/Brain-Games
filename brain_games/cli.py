73626bf9c78bb998f49513664b8b046a0b09bbae
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-02-18 12:41:45 +0900

    add congratulations and try_again functions

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,12 +3,23 @@ import prompt
 
 def welcome_user():
     print("Welcome to the Brain Games!")
+    global name
     name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}')
+    print(f'Hello, {name}!')
+
+
+def congratulate_user():
+    print(f'Congratulations, {name}!')
+
+
+def try_again_user():
+    print(f"Let's try again, {name}!")
 
 
 def main():
     welcome_user()
+    congratulate_user()
+    try_again_user()
 
 
 if __name__ == "__main__":
