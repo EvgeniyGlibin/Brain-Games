63c50886e23f0aa6ecb6fca26227e40f8d8f49db
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-02-19 14:04:12 +0900

    update the logic of the game

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,38 +1,39 @@
 #!/usr/bin/env python3
 
 from brain_games.cli import welcome_user
-from brain_games.cli import congratulate_user
-from brain_games.cli import try_again_user
+from brain_games.cli import is_question
+from brain_games.cli import is_answer
+from brain_games.cli import is_correct
 
-from random import randint
-import prompt
+from brain_games.cli import congratulate_user
+from brain_games.cli import is_wrong_answer
+from brain_games.cli import try_again_user
+from brain_games.games.even import play_even
+from brain_games.games.even import random_digit
+
+
+#from random import randint
+#import prompt
 
 
 def is_even():
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    count = 0
-    while count < 3:
-        digit = randint(1, 100)
-        print(f'Question: {digit}')
-        print('You answer: ', end='')
-        answer = input()
-        if digit % 2 == 0:
-            if answer == "yes":
-                print('Correct!')
-                count += 1
-            else:
-                print(f"'no' is wrong answer ;(. Correct answer was 'yes'.")
-                try_again_user()
-                break
-        if digit % 2 == 1:
-            if answer == "no":
-                print('Correct!')
-                count += 1
-            else:
-                print(f"'yes' is wrong answer ;(. Correct answer was 'no'.")
-                try_again_user()
-                break
-        congratulate_user()
+    count = 3
+    while count > 0:
+        is_question()
+        random_digit()
+        is_answer()
+        entered_response = input()
+        if entered_response == play_even():
+            is_correct()
+            count -= 1
+        else:
+            is_wrong_answer(entered_response, play_even())
+            try_again_user()
+            break
+    else:
+        congratulate_user()       
+
 
 
 def main():
