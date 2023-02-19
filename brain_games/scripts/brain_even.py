ba74a33c7b788452218935794f114a2c3eeebb0f
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-02-20 07:09:49 +0900

    delet comments

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,32 +3,6 @@
 
 from brain_games.cli import welcome_user
 from brain_games.games.even import is_even
-# from brain_games.cli import is_question
-# from brain_games.cli import is_answer
-# from brain_games.cli import is_correct
-# from brain_games.cli import congratulate_user
-# from brain_games.cli import is_wrong_answer
-# from brain_games.cli import try_again_user
-# from brain_games.games.even import play_even
-# from brain_games.games.even import random_digit
-
-
-# def is_even():
-#     print('Answer "yes" if the number is even, otherwise answer "no".')
-#     count = 3
-#     while count > 0:
-#         is_question(random_digit())
-#         print(is_answer(), end='')
-#         entered_response = input()
-#         if entered_response == play_even():
-#             is_correct()
-#             count -= 1
-#         else:
-#             is_wrong_answer(entered_response, play_even())
-#             try_again_user()
-#             break
-#     else:
-#         congratulate_user()
 
 
 def main():
