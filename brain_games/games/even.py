329d6aded2e13ae03f78857b388a1a6ec7de8a60
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-02-20 07:04:16 +0900

    add logic game

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,10 +1,15 @@
 from random import randint
-
+from brain_games.cli import is_question
+from brain_games.cli import is_answer
+from brain_games.cli import is_correct
+from brain_games.cli import is_wrong_answer
+from brain_games.cli import try_again_user
+from brain_games.cli import congratulate_user
 
 def random_digit():
     global digit
     digit = randint(1, 100)
-    print(digit)
+    return digit
 
 
 def play_even():
@@ -15,9 +20,29 @@ def play_even():
         return 'yes'
 
 
+def is_even():
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    count = 3
+    while count > 0:
+        is_question(random_digit())
+        print(is_answer(), end='')
+        entered_response = input()
+        if entered_response == play_even():
+            is_correct()
+            count -= 1
+        else:
+            is_wrong_answer(entered_response, play_even())
+            try_again_user()
+            break
+    else:
+        congratulate_user()
+
+
 def main():
     random_digit()
     play_even()
+    is_even
+
 
 
 if __name__ == '__main__':
