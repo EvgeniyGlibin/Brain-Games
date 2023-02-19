9fda3bbcdf6ac40c5e6ebbdde6fc794aef1693c1
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-02-19 13:07:13 +0900

    add new file even.py

---
+++ brain_games/games/even.py
@@ -0,0 +1,23 @@
+from random import randint
+
+def random_digit():
+    global digit
+    digit = randint(1, 100)
+    print(digit)
+
+
+def play_even():
+    answer = digit % 2
+    if answer == 1:
+        return 'no'
+    if answer == 0:
+        return 'yes'
+
+
+def main():
+    random_digit()
+    play_even()
+
+
+if __name__ == '__main__':
+    main()
