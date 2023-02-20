374feaa8ceeb566ce001547057f9fda147cab747
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-02-21 07:45:59 +0900

    find a solution

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -4,12 +4,14 @@
 from brain_games.cli import welcome_user
 from brain_games.cli import congratulate_user
 from brain_games.games.calc import is_calc
-
+from brain_games.games.calc import random_expression
 
 def main():
     welcome_user()
     is_calc()
     congratulate_user()
+    random_expression()
+    
 
 
 if __name__ == '__main__':
