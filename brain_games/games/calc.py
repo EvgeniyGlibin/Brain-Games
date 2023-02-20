879b4b308401d4e9582f2f2394ad282e100be09c
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-02-21 07:08:32 +0900

    add new file calc.py

---
+++ brain_games/games/calc.py
@@ -0,0 +1,14 @@
+#from random import randint
+from brain_games.cli import is_question
+
+
+def is_calc():
+    print('What is the result of the expression?')
+
+
+def main():
+    is_calc()
+
+
+if __name__ == "__main__":
+    main()
