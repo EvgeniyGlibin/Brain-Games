e41ee9e8d59eea100a57dd1159e7cf3803f79d35
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-02-22 07:51:07 +0900

    update the game logic

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,21 +1,39 @@
 from random import randint
-from random import choices
 from brain_games.cli import is_question
+from brain_games.cli import is_answer
+from brain_games.cli import is_correct
+from brain_games.cli import is_wrong_answer
+from brain_games.cli import try_again_user
+from brain_games.cli import congratulate_user
 
 
 def random_expression():
     first_number = randint(1, 30)
-    second_namber = randint(1, 15)
+    second_number = randint(1, 15)
     list_arithmetic_operations = [' + ', ' - ', ' * ']
-    arithmetic_operation = list_arithmetic_operations[randint(0, len(list_arithmetic_operations) - 1)]
-    #random_operation = choices(('+', '-', '*'))
-    print(str(first_number) + arithmetic_operation + str(second_namber))
-    #print(type(arithmetic_operation))
-
+    arithmetic_operation = list_arithmetic_operations[
+        randint(0, len(list_arithmetic_operations) - 1)]
+    global arithmetic_expression
+    arithmetic_expression = f"{first_number} {arithmetic_operation} {second_number}"
+    return arithmetic_expression
 
 
 def is_calc():
     print('What is the result of the expression?')
+    count = 3
+    while count > 0:
+        is_question(random_expression())
+        print(is_answer(), end='')
+        entered_response = input()
+        if int(entered_response) == eval(arithmetic_expression):
+            is_correct()
+            count -= 1
+        else:
+            is_wrong_answer(entered_response, eval(arithmetic_expression))
+            try_again_user()
+            break
+    else:
+        congratulate_user()
 
 
 def main():
