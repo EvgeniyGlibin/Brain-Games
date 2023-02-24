89219af20191711bee451914bd651904d99cdd3b
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-02-24 12:39:34 +0900

    change logic game

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,2 +1,13 @@
+from random import randint
+
+
+def is_random():
+    global digit_random_1
+    digit_random_1 = randint(1, 100)
+    global digit_random_2
+    digit_random_2 = randint(1, 100)
+    return f'{digit_random_1} {digit_random_2}'
+
+
 def is_progression():
     print("Начинаем игру")
