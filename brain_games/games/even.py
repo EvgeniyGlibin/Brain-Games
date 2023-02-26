134ce00fd102a6f7ff1f5d8b91b1b1814626d355
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-02-27 07:38:52 +0900

    fix function names

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,20 +1,20 @@
 from random import randint
-from brain_games.cli import is_question
-from brain_games.cli import is_answer
-from brain_games.cli import is_correct
-from brain_games.cli import is_wrong_answer
+from brain_games.cli import ask_question
+from brain_games.cli import enter_response
+from brain_games.cli import confirm_response
+from brain_games.cli import report_an_error
 from brain_games.cli import try_again_user
 from brain_games.cli import congratulate_user
 from brain_games.cli import NUMBER_OF_ATTEPTS
 
 
-def random_digit():
+def get_random_number():
     global digit
     digit = randint(1, 100)
     return digit
 
 
-def play_even():
+def check_parity():
     answer = digit % 2
     if answer == 1:
         return 'no'
@@ -22,16 +22,16 @@ def play_even():
         return 'yes'
 
 
-def is_even():
+def play_even():
     print('Answer "yes" if the number is even, otherwise answer "no".')
     for _ in range(NUMBER_OF_ATTEPTS):
-        is_question(random_digit())
-        print(is_answer(), end='')
+        ask_question(get_random_number())
+        print(enter_response(), end='')
         entered_response = input()
-        if entered_response == play_even():
-            is_correct()
+        if entered_response == check_parity():
+            confirm_response()
         else:
-            is_wrong_answer(entered_response, play_even())
+            report_an_error(entered_response, check_parity())
             try_again_user()
             break
     else:
@@ -39,9 +39,9 @@ def is_even():
 
 
 def main():
-    random_digit()
+    get_random_number()
+    check_parity()
     play_even()
-    is_even
 
 
 if __name__ == '__main__':
