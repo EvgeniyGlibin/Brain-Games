d54affcbe31dcb6e9887bc67d6a929b195436ff3
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-02-26 18:08:30 +0900

    fix bugs

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -5,12 +5,12 @@ from brain_games.cli import is_correct
 from brain_games.cli import is_wrong_answer
 from brain_games.cli import try_again_user
 from brain_games.cli import congratulate_user
-
+from brain_games.cli import NUMBER_OF_ATTEPTS
 
 def random_expression():
     first_number = randint(1, 30)
     second_number = randint(1, 15)
-    list_arithmetic_operations = [' + ', ' - ', ' * ']
+    list_arithmetic_operations = ['+', '-', '*']
     arithmetic_operation = list_arithmetic_operations[
         randint(0, len(list_arithmetic_operations) - 1)]
     global arithmetic_expression
@@ -20,14 +20,12 @@ def random_expression():
 
 def is_calc():
     print('What is the result of the expression?')
-    count = 3
-    while count > 0:
+    for _ in range(NUMBER_OF_ATTEPTS):
         is_question(random_expression())
         print(is_answer(), end='')
         entered_response = input()
         if int(entered_response) == eval(arithmetic_expression):
             is_correct()
-            count -= 1
         else:
             is_wrong_answer(entered_response, eval(arithmetic_expression))
             try_again_user()
