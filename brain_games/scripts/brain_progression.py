efd3bcf6323a5f2a618c22d637728a0738ffb2ab
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-02-26 10:55:29 +0900

    add logic scripts/brain_progression

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+from brain_games.cli import welcome_user
 from brain_games.games.progression import is_progression
 
 
 def main():
-    print('арифметическая прогрессия')
+    welcome_user()
     is_progression()
 
 
