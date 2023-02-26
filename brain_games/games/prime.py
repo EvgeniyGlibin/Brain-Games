359ba838372e4fdc7048cc83be9f57274f851bd9
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-02-27 07:56:56 +0900

    fix function names

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -14,7 +14,7 @@ def get_random_number():
     return digit
 
 
-def play_prime():
+def check_the_number_prime():
     count = 0
     for i in range(2, digit // 2 + 1):
         if digit % i == 0:
@@ -24,16 +24,16 @@ def play_prime():
     return 'no'
 
 
-def is_prime():
+def play_prime():
     print('Answer "yes" if given number is prime. Otherwise answer "no"')
     for _ in range(NUMBER_OF_ATTEPTS):
         ask_question(get_random_number())
         print(enter_response(), end='')
         entered_response = input()
-        if entered_response == play_prime():
+        if entered_response == check_the_number_prime():
             confirm_response()
         else:
-            report_an_error(entered_response, play_prime())
+            report_an_error(entered_response, check_the_number_prime())
             try_again_user()
             break
     else:
