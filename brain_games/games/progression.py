ffaeb6900d32de1d616368254ef0cd236c685ef3
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-02-26 10:55:01 +0900

    add logic game

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,13 +1,44 @@
 from random import randint
+from brain_games.cli import is_question
+from brain_games.cli import is_answer
+from brain_games.cli import is_correct
+from brain_games.cli import is_wrong_answer
+from brain_games.cli import try_again_user
+from brain_games.cli import congratulate_user
 
+
+NUMBER_OF_ATTEPTS = 3
 
 def is_random():
-    global digit_random_1
-    digit_random_1 = randint(1, 100)
-    global digit_random_2
-    digit_random_2 = randint(1, 100)
-    return f'{digit_random_1} {digit_random_2}'
+    digit_random = randint(1, 50)
+    return digit_random
+
+def arifmetic_progression():
+    first_element = is_random()
+    second_element = is_random()
+    step_progression = second_element - first_element
+    list_of_elements = []
+    for _ in range(randint(5, 10)):
+        list_of_elements.append(str(first_element))
+        first_element += step_progression
+    random_list_element = randint(0, len(list_of_elements) - 1)
+    global hidden_element
+    hidden_element = list_of_elements.pop(random_list_element)
+    list_of_elements.insert(random_list_element,'..')
+    return ' '.join(list_of_elements)
 
 
 def is_progression():
-    print("Начинаем игру")
+    print('What number is missing in the progression?')
+    for i in range(NUMBER_OF_ATTEPTS):
+        is_question(arifmetic_progression())
+        print(is_answer(), end='')
+        entered_response = input()
+        if entered_response == hidden_element:
+            is_correct()
+        else:
+            is_wrong_answer(entered_response, hidden_element)
+            try_again_user
+            break
+    else:
+        congratulate_user()
