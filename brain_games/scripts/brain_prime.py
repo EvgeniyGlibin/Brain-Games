90e5eb372bda4a897d1ead8983d06733a3cfd502
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-02-26 11:55:15 +0900

    fix logic game

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -2,10 +2,11 @@
 
 
 from brain_games.games.prime import is_prime
-
+from brain_games.games.prime import play_prime
 
 def main():
     is_prime()
+    print(play_prime(2))
 
 
 if __name__ == "__main__":
