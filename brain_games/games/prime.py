df532be8578262f8fb237b108043fe9580f79ef6
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-02-26 11:54:42 +0900

    add function play_prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,2 +1,12 @@
 def is_prime():
     print('Игра простое число')
+
+
+def play_prime(x):
+    count = 0
+    for i in range(2, x//2 + 1):
+        if x % i == 0:
+            count += 1
+    if count == 0:
+        return 'yes'
+    return 'no'
