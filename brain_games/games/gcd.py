46625445634452f9fbbc539e5397b30ece0c13f9
EvgeniyGlibin <evgeniyglibin95@gmail.com>
2023-02-27 15:25:43 +0900

    fix function names

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -9,36 +9,35 @@ from brain_games.cli import NUMBER_OF_ATTEPTS
 
 
 def get_random_number():
-    digit_random_1 = randint(1, 100)
-    digit_random_2 = randint(1, 100)
-    global digit_random
-    digit_random = f'{digit_random_1} {digit_random_2}'
-    return digit_random
+    global digit_random_1
+    global digit_random_2
+    digit_random_1 = randint(1, 50)
+    digit_random_2 = randint(1, 50)
+    return f'{digit_random_1} {digit_random_2}'
 
 
-def is_divider(number='3'):
+def get_divisors(number):
     number = int(number)
-    lst = [number]
+    list_of_divisors = [number]
     for i in range(1, number // 2 + 1):
         if number % i == 0:
-            lst.append(i)
-    return sorted(lst)
+            list_of_divisors.append(i)
+    return sorted(list_of_divisors)
 
 
-def is_list(index_list_0=[], index_list_1=[]):
-    list_1 = is_divider(index_list_0)
-    list_2 = is_divider(index_list_1)
-    for i in range(1, len(list_1) + 1):
-        if list_1[-i] in list_2:
-            return list_1[-i]
+def get_max_common_divisor(first_number, second_number):
+    list_of_divisors_1 = get_divisors(first_number)
+    list_of_divisors_2 = get_divisors(second_number)
+    for i in range(1, len(list_of_divisors_1) + 1):
+        if list_of_divisors_1[-i] in list_of_divisors_2:
+            return list_of_divisors_1[-i]
 
 
-def is_gcd():
+def play_gcd():
     print('Find the greatest common divisor of given numbers.')
     for _ in range(NUMBER_OF_ATTEPTS):
         ask_question(get_random_number())
-        lst_digit = digit_random.split()
-        correct_answer = is_list(lst_digit[0], lst_digit[1])
+        correct_answer = get_max_common_divisor(digit_random_1, digit_random_2)
         print(enter_response(), end='')
         entered_response = input()
         if int(entered_response) == correct_answer:
