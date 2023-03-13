85eeaa7128a78c12e17091228f47930f34fcf9bf
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-03-14 07:47:35 +0900

    change logic game

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,38 +1,41 @@
 from random import randint
-from brain_games.cli import ask_question
-from brain_games.cli import enter_response
-from brain_games.cli import confirm_response
-from brain_games.cli import report_an_error
-from brain_games.cli import try_again_user
-from brain_games.cli import congratulate_user
-from brain_games.cli import NUMBER_OF_ATTEPTS
+# from brain_games.cli import ask_question
+# from brain_games.cli import enter_response
+# from brain_games.cli import confirm_response
+# from brain_games.cli import report_an_error
+# from brain_games.cli import try_again_user
+# from brain_games.cli import congratulate_user
+# from brain_games.cli import NUMBER_OF_ATTEPTS
 
 
-def get_random_number():
-    global digit
-    digit = randint(1, 100)
-    return digit
+GAME_DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+# def get_random_number():
+#     digit = randint(1, 100)
+#     return digit
 
 
 def check_parity():
-    answer = digit % 2
+    random_digit = randint(1, 100)
+    answer = random_digit % 2
     if answer == 1:
-        return 'no'
+        answer_game = 'no'
     if answer == 0:
-        return 'yes'
+        answer_game = 'yes'
+    return random_digit, answer_game
 
 
-def play_even():
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    for _ in range(NUMBER_OF_ATTEPTS):
-        ask_question(get_random_number())
-        print(enter_response(), end='')
-        entered_response = input()
-        if entered_response == check_parity():
-            confirm_response()
-        else:
-            report_an_error(entered_response, check_parity())
-            try_again_user()
-            break
-    else:
-        congratulate_user()
+def show_game_description(game):
+    return game.GAME_DESCRIPTION
+
+
+
+
+def main():
+    check_parity()
+    
+
+
+if __name__ == "__main__":
+    main()
+
