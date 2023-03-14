0b8f197e00245da9bcc2588500ed3edd98191991
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-03-15 07:54:22 +0900

    change the game logic

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,40 +1,23 @@
 from random import randint
-from brain_games.cli import ask_question
-from brain_games.cli import enter_response
-from brain_games.cli import confirm_response
-from brain_games.cli import report_an_error
-from brain_games.cli import try_again_user
-from brain_games.cli import congratulate_user
-from brain_games.cli import NUMBER_OF_ATTEPTS
 
 
+GAME_DESCRIPTION = "описание на простое число"
 def get_random_number():
     global digit
     digit = randint(1, 100)
     return digit
 
 
-def check_the_number_prime():
+def play_the_game():
+    digit = randint(1, 100)
     count = 0
     for i in range(2, digit // 2 + 1):
         if digit % i == 0:
             count += 1
     if count == 0 and digit != 1:
-        return 'yes'
-    return 'no'
-
-
-def play_prime():
-    print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    for _ in range(NUMBER_OF_ATTEPTS):
-        ask_question(get_random_number())
-        print(enter_response(), end='')
-        entered_response = input()
-        if entered_response == check_the_number_prime():
-            confirm_response()
-        else:
-            report_an_error(entered_response, check_the_number_prime())
-            try_again_user()
-            break
+        answer_game = 'yes'
     else:
-        congratulate_user()
+        answer_game = 'no'
+    return digit, answer_game
+
+
