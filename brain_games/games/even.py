6d531ea2d6dc526e5b49045a3aa225e57ad83efa
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-03-15 07:31:30 +0900

    fix the logic of the game

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,18 +1,7 @@
 from random import randint
-# from brain_games.cli import ask_question
-# from brain_games.cli import enter_response
-# from brain_games.cli import confirm_response
-# from brain_games.cli import report_an_error
-# from brain_games.cli import try_again_user
-# from brain_games.cli import congratulate_user
-# from brain_games.cli import NUMBER_OF_ATTEPTS
 
 
 GAME_DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
-
-# def get_random_number():
-#     digit = randint(1, 100)
-#     return digit
 
 
 def check_parity():
@@ -25,17 +14,9 @@ def check_parity():
     return random_digit, answer_game
 
 
-def show_game_description(game):
-    return game.GAME_DESCRIPTION
-
-
-
-
 def main():
     check_parity()
-    
 
 
 if __name__ == "__main__":
     main()
-
