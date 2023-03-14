ceff6cda53362be691e2ec62fcc339a4d9a9f4a2
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-03-15 07:27:41 +0900

    change functions

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,34 +1,23 @@
 import prompt
 
 
-NUMBER_OF_ATTEPTS = 3
-
-
 def welcome_user():
     print("Welcome to the Brain Games!")
-    # global name
-    # name = prompt.string('May I have your name? ')
-    # print(f'Hello, {name}!')
+
 
 def enter_name():
     global name
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-    # return name
 
 
-def ask_question(condition='question'):
+def ask_question(condition):
     print(f'Question: {condition}')
 
 
 def enter_answer():
-    global entered_answer
     entered_answer = prompt.string('Your answer: ')
     return entered_answer
-
-
-def enter_response():
-    return 'Your answer: '
 
 
 def confirm_response():
@@ -39,8 +28,9 @@ def congratulate_user():
     print(f'Congratulations, {name}!')
 
 
-def report_an_error(x='no', y='yes'):
-    print(f"'{x}' is wrong answer ;(. Correct answer was '{y}'.")
+def report_an_error(entered_answer, correct_answer):
+    print(f"'{entered_answer}' is wrong answer ;(.", end=' ')
+    print(f"Correct answer was '{correct_answer}'.")
 
 
 def try_again_user():
@@ -50,12 +40,10 @@ def try_again_user():
 def main():
     welcome_user()
     ask_question()
-    enter_response()
     confirm_response()
     congratulate_user()
     report_an_error()
     try_again_user()
-    NUMBER_OF_ATTEPTS
 
 
 if __name__ == "__main__":
