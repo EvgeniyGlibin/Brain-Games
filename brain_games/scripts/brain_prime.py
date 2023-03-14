49066d41d868a4b0475e32eed7c7fd8b2eed77fe
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-03-15 07:53:56 +0900

    change the game logic

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
 
-from brain_games.cli import welcome_user
-from brain_games.games.prime import play_prime
+from brain_games.game_engine import start_game
+import brain_games.games.prime
 
 
 def main():
-    welcome_user()
-    play_prime()
+    start_game(brain_games.games.prime)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
