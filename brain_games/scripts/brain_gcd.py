8c3108dbc60a29117334ed58b9a31ae82b5c68cf
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-03-17 07:40:34 +0900

    fix the script logic

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-from brain_games.cli import welcome_user
-from brain_games.games.gcd import play_gcd
+
+from brain_games.game_engine import start_game
+import brain_games.games.gcd
 
 
 def main():
-    welcome_user()
-    play_gcd()
+    start_game(brain_games.games.gcd)
 
 
 if __name__ == '__main__':
