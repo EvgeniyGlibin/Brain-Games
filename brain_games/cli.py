435d5c63d5bf0c87353a6313f0fb005d57778e99
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-03-17 08:21:46 +0900

    fix function welcome_user

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,12 +3,14 @@ import prompt
 
 def welcome_user():
     print("Welcome to the Brain Games!")
-
-
-def enter_name():
     global name
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
+
+# def enter_name():
+#     global name
+#     name = prompt.string('May I have your name? ')
+#     print(f'Hello, {name}!')
 
 
 def ask_question(condition):
