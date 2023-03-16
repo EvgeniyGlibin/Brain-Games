d6c6d0142dedfc33f997abb6ed207df5efc109c4
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-03-17 08:36:47 +0900

    fix bug

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-GAME_DESCRIPTION = 'Answer "yes" if given number is prime.' \
+GAME_DESCRIPTION = 'Answer "yes" if given number is prime. ' \
     'Otherwise answer "no".'
 
 
