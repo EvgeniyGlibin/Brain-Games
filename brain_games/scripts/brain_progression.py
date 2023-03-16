ee43844f0569539efec40bda87fc8af65e30b2e7
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-03-17 07:50:20 +0900

    fix the script logic

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-from brain_games.cli import welcome_user
-from brain_games.games.progression import play_progression
+
+
+from brain_games.game_engine import start_game
+import brain_games.games.progression
 
 
 def main():
-    welcome_user()
-    play_progression()
+    start_game(brain_games.games.progression)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
