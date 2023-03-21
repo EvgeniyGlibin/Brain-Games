c17d351b5924a3470be729d984c83b95491eadc9
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-03-22 07:02:54 +0900

    remove the get_random_number function

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -4,14 +4,9 @@ from random import randint
 GAME_DESCRIPTION = 'What is the result of the expression?'
 
 
-def get_random_number():
-    digit_random = randint(1, 50)
-    return digit_random
-
-
 def set_expression():
-    digit_1 = get_random_number()
-    digit_2 = get_random_number()
+    digit_1 = randint()
+    digit_2 = randint()
     arithmetic_operations = ['+', '-', '*']
     random_index = randint(0, len(arithmetic_operations) - 1)
     arithmetic_operation = arithmetic_operations[random_index]
