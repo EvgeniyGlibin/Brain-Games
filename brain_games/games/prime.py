797e84bf4a913fa31e79d85ce6911ea172bb39fc
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-03-23 08:06:51 +0900

    fix the logic of the game

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,14 +7,23 @@ MIN_VALUE = 1
 MAX_VALUE = 100
 
 
-def play_the_game():
-    digit = randint(MIN_VALUE, MAX_VALUE)
+def get_random_digit():
+    random_digit = randint(MIN_VALUE, MAX_VALUE)
+    return random_digit
+
+
+def check_for_prime(digit):
+    answer = 'no'
     count = 0
     for i in range(2, digit // 2 + 1):
         if digit % i == 0:
             count += 1
     if count == 0 and digit != 1:
-        answer_game = 'yes'
-    else:
-        answer_game = 'no'
-    return digit, answer_game
+        answer = 'yes'
+    return answer
+
+
+def play_the_game():
+    question = get_random_digit()
+    correct_answer = check_for_prime(question)
+    return question, correct_answer
