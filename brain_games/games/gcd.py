a85c2d3d39554f76a1629907865a58c250212d37
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-03-23 08:06:39 +0900

    fix the logic of the game

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -5,23 +5,24 @@ GAME_DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 MIN_VALUE = 1
 MAX_VALUE = 50
 
-def get_random_number():
-    digit_random = randint(MIN_VALUE, MAX_VALUE)
-    return digit_random
+
+def get_random_digit():
+    random_digit = randint(MIN_VALUE, MAX_VALUE)
+    return random_digit
 
 
 def get_divisors(number):
-    list_of_divisors = [number]
+    list_divisors = [number]
     for i in range(1, number // 2 + 1):
         if number % i == 0:
-            list_of_divisors.append(i)
-    list_divisors = sorted(list_of_divisors)
-    return list_divisors
+            list_divisors.append(i)
+    sorted_list_divisors = sorted(list_divisors)
+    return sorted_list_divisors
 
 
 def play_the_game():
-    first_number = get_random_number()
-    second_number = get_random_number()
+    first_number = get_random_digit()
+    second_number = get_random_digit()
     list_of_divisors_1 = get_divisors(first_number)
     list_of_divisors_2 = get_divisors(second_number)
     for i in range(1, len(list_of_divisors_1) + 1):
@@ -30,3 +31,13 @@ def play_the_game():
             correct_answer = str(max_common_divisor)
             question = f'{first_number} {second_number}'
             return question, correct_answer
+
+
+# def get_gcd(a, b):
+#     first_digit = max(a, b)
+#     second_digit = min(a, b)
+#     remains = 1
+#     while remains > 0:
+#         remains = first_digit % second_digit
+#         first_digit, second_digit = second_digit, remains
+#     return first_digit
