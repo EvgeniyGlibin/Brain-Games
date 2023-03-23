47ce1e0a5173dfb88cdae8ea370065d3cb913cf7
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-03-24 07:58:12 +0900

    rename a functions and fix bugs

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,4 +1,4 @@
-from random import randint
+import random
 
 
 GAME_DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
@@ -7,7 +7,7 @@ MAX_VALUE = 100
 
 
 def get_random_digit():
-    random_digit = randint(MIN_VALUE, MAX_VALUE)
+    random_digit = random.randint(MIN_VALUE, MAX_VALUE)
     return random_digit
 
 
@@ -18,7 +18,7 @@ def check_for_even(digit):
     return answer
 
 
-def play_the_game():
+def get_game_values():
     question = get_random_digit()
     correct_answer = check_for_even(question)
     return question, correct_answer
