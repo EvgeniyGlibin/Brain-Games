4797351edf81334ae0ae35b72603c4ad38916196
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-03-24 07:57:01 +0900

    rename a function and fix bugs

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,4 +1,4 @@
-from random import randint
+import random
 
 
 GAME_DESCRIPTION = 'What is the result of the expression?'
@@ -7,16 +7,16 @@ MAX_VALUE = 50
 
 
 def set_expression():
-    digit_1 = randint(MIN_VALUE, MAX_VALUE)
-    digit_2 = randint(MIN_VALUE, MAX_VALUE)
+    digit_1 = random.randint(MIN_VALUE, MAX_VALUE)
+    digit_2 = random.randint(MIN_VALUE, MAX_VALUE)
     arithmetic_operations = ['+', '-', '*']
-    random_index = randint(0, len(arithmetic_operations) - 1)
+    random_index = random.randint(0, len(arithmetic_operations) - 1)
     arithmetic_operation = arithmetic_operations[random_index]
     expression = f"{digit_1} {arithmetic_operation} {digit_2}"
     return expression
 
 
-def play_the_game():
+def get_game_values():
     question = set_expression()
     correct_answer = str(eval(question))
     return question, correct_answer
