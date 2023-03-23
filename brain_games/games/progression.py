fe36496c4f0a4c61a3b6edc27110fd58b16b0a04
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-03-24 08:01:46 +0900

    rename a functions and fix bugs

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,29 +1,30 @@
-from random import randint
+import random
 
 
 GAME_DESCRIPTION = 'What number is missing in the progression?'
 MIN_VALUE = 1
 MAX_VALUE = 50
-MIN_LENGTH_PROGRESSION = 5
-MAX_LENGTH_PROGRESSION = 10
+MIN_LENGTH = 5
+MAX_LENGTH = 10
 
 
-def get_random_number():
-    digit_random = randint(MIN_VALUE, MAX_VALUE)
-    return digit_random
-
-
-def play_the_game():
-    initial_term = get_random_number()
-    succeeding_term = get_random_number()
+def get_arithmetic_progression():
+    initial_term = random.randint(MIN_VALUE, MAX_VALUE)
+    succeeding_term = random.randint(MIN_VALUE, MAX_VALUE)
     common_difference = succeeding_term - initial_term
     arithmetic_progression = []
-    for _ in range(randint(MIN_LENGTH_PROGRESSION, MAX_LENGTH_PROGRESSION)):
+    lenght_arithmetic_progression = random.randint(MIN_LENGTH, MAX_LENGTH)
+    for _ in range(lenght_arithmetic_progression):
         arithmetic_progression.append(str(initial_term))
         initial_term += common_difference
-    random_tern = randint(0, len(arithmetic_progression) - 1)
-    hidden_element = arithmetic_progression.pop(random_tern)
-    arithmetic_progression.insert(random_tern, '..')
-    question = ' '.join(arithmetic_progression)
+    return arithmetic_progression
+
+
+def get_game_values():
+    progression = get_arithmetic_progression()
+    random_tern = random.randint(0, len(progression) - 1)
+    hidden_element = progression.pop(random_tern)
+    progression.insert(random_tern, '..')
+    question = ' '.join(progression)
     correct_answer = hidden_element
     return question, correct_answer
