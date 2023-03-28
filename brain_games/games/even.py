5dbf9ae6ac8a47f7ffdf92e0fc0a196ff426ce4d
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-03-29 08:11:37 +0900

    change logic fynctions

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -12,13 +12,22 @@ def get_random_digit():
 
 
 def check_for_even(digit):
-    answer = 'yes'
+    result = True
     if digit % 2 == 1:
-        answer = 'no'
-    return answer
+        result = False
+    return result
+
+
+# def is_even(digit):
+#     if check_for_even(digit):
+#         return 'yes'
+#     else:
+#         return 'no'
 
 
 def get_game_values():
     question = get_random_digit()
-    correct_answer = check_for_even(question)
+    # correct_answer = is_even(question)
+    # как лучше, через тернарное условие или заменить на функцию is_even()?
+    correct_answer = 'yes' if check_for_even(question) else 'no'
     return question, correct_answer
