0fec411fe295025a6da9b5634239bba5443afab1
Evgeniy Glibin <evgeniyglibin95@gmail.com>
2023-03-30 07:58:08 +0900

    change logic function

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -8,23 +8,23 @@ MIN_LENGTH = 5
 MAX_LENGTH = 10
 
 
-def get_arithmetic_progression():
-    initial_term = random.randint(MIN_VALUE, MAX_VALUE)
-    succeeding_term = random.randint(MIN_VALUE, MAX_VALUE)
+def get_arithmetic_progression(initial_term, succeeding_term, lenght):
     common_difference = succeeding_term - initial_term
     arithmetic_progression = []
-    lenght_arithmetic_progression = random.randint(MIN_LENGTH, MAX_LENGTH)
-    for _ in range(lenght_arithmetic_progression):
+    for _ in range(lenght):
         arithmetic_progression.append(str(initial_term))
         initial_term += common_difference
     return arithmetic_progression
 
 
 def get_values():
-    progression = get_arithmetic_progression()
+    initial_term = random.randint(MIN_VALUE, MAX_VALUE)
+    succeeding_term = random.randint(MIN_VALUE, MAX_VALUE)
+    lenght = random.randint(MIN_LENGTH, MAX_LENGTH)
+    progression = get_arithmetic_progression(initial_term, succeeding_term, lenght)
     random_tern = random.randint(0, len(progression) - 1)
-    hidden_element = progression.pop(random_tern)
-    progression.insert(random_tern, '..')
+    hidden_element = progression[random_tern]
+    progression[random_tern] = '..'
     question = ' '.join(progression)
     correct_answer = hidden_element
     return question, correct_answer
