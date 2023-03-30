26f9ea57a95a2270fb3ee9dd3cb340206216be43
EvgeniyGlibin <evgeniyglibin95@gmail.com>
2023-03-30 17:18:39 +0900

    change logic game

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -6,17 +6,17 @@ MIN_VALUE = 1
 MAX_VALUE = 50
 
 
-def set_expression():
-    digit_1 = random.randint(MIN_VALUE, MAX_VALUE)
-    digit_2 = random.randint(MIN_VALUE, MAX_VALUE)
+def set_expression(first_number, second_number):
     arithmetic_operations = ['+', '-', '*']
     random_index = random.randint(0, len(arithmetic_operations) - 1)
     arithmetic_operation = arithmetic_operations[random_index]
-    expression = f"{digit_1} {arithmetic_operation} {digit_2}"
+    expression = f"{first_number} {arithmetic_operation} {second_number}"
     return expression
 
 
 def get_values():
-    question = set_expression()
+    first_number = random.randint(MIN_VALUE, MAX_VALUE)
+    second_number = random.randint(MIN_VALUE, MAX_VALUE)
+    question = set_expression(first_number, second_number)
     correct_answer = str(eval(question))
     return question, correct_answer
